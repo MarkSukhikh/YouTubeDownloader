48614ffb2355101be84ba81722af6c634990484c
MarkSukhikh <suhih.mark@gmail.com>
2022-08-27 09:09:54 +0800

    Add download button

--- YoutubeDownloadGUI.py
+++ YoutubeDownloadGUI.py
@@ -1,27 +1,43 @@
+from audioop import add
 import sys
+from unittest.mock import sentinel
 from pytube import YouTube
 import dearpygui.dearpygui as dpg
 
+
 link = ''
+tags_set = set([])
+
+def download_stream_callback(sender, data):
+    yt = YouTube(link)
+
+    for id_tag in tags_set:
+        stream = yt.streams.get_by_itag(id_tag)
+        stream.download(filename_prefix=id_tag+"_")
+
+def checkbox_clic_callback(sender, data):
+    global tags_set
+    if data:
+        tags_set.add(sender)
+    else:
+        tags_set.remove(sender)
 
 def check_streams_callback(sender, data):
-    global link, new_checkbox
+    global link, new_checkbox, new_button
     yt = YouTube(link)
     streams = yt.streams
     for i in streams:
         stream_str = str(i)
         id_tag = stream_str[15: 18]
         id_tag = id_tag.replace('"','')
-        new_checkbox = dpg.add_checkbox(label=i, before="download_button", tag=id_tag)
-        
+        new_checkbox = dpg.add_checkbox(label=i, callback=checkbox_clic_callback, before="end", tag=id_tag)
+    new_button = dpg.add_button(label="Download stream", callback=download_stream_callback,  before="end", tag="download_button")
 
 
 def save_link_callback(sender, data):
     global link
     link = str(data)
 
-def download_stream_callback(sender, data):
-    pass
 
 dpg.create_context()
 dpg.create_viewport(title='Youtube downloader', width=600, height=300)
@@ -30,8 +46,9 @@ with dpg.window(tag="pw", label="Youtube downloader"):
     dpg.add_text("Insert link below, check the streams and download!")
     dpg.add_text("Paste Youtube link here:")
     dpg.add_input_text(default_value="https://", callback=save_link_callback)
-    dpg.add_button(label="Click here to check the streams", callback=check_streams_callback)
-    dpg.add_button(label="Download stream", callback=download_stream_callback, tag="download_button", show=False)
+    dpg.add_button(label="Click here to check the streams", callback=check_streams_callback, tag="stream_button")
+    dpg.add_text("", tag="end")
+#    dpg.add_button(label="Download stream", callback=download_stream_callback, tag="download_button")
 
 
 
