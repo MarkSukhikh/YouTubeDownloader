4ac0460357685691ddc0770b6d1ef66600e9871a
Mark <suhih.mark@gmail.com>
2025-06-09 00:36:25 +0300

    pytubefix

--- YoutubeDownloadGUI.py
+++ YoutubeDownloadGUI.py
@@ -1,7 +1,7 @@
-from audioop import add
+#from audioop import add
 import sys
 from unittest.mock import sentinel
-from pytube import YouTube
+from pytubefix import YouTube
 import dearpygui.dearpygui as dpg
 
 
@@ -9,7 +9,8 @@ link = ''
 tags_set = set([])
 
 def download_stream_callback(sender, data):
-    yt = YouTube(link)
+
+    yt = YouTube(link, use_oauth=True, allow_oauth_cache=True)
 
     for id_tag in tags_set:
         stream = yt.streams.get_by_itag(id_tag)
@@ -24,13 +25,13 @@ def checkbox_clic_callback(sender, data):
 
 def check_streams_callback(sender, data):
     global link, new_checkbox, new_button
-    yt = YouTube(link)
+    yt = YouTube(link, use_oauth=True, allow_oauth_cache=True)
     streams = yt.streams
     for i in streams:
         stream_str = str(i)
         id_tag = stream_str[15: 18]
         id_tag = id_tag.replace('"','')
-        new_checkbox = dpg.add_checkbox(label=i, callback=checkbox_clic_callback, before="end", tag=id_tag)
+        new_checkbox = dpg.add_checkbox(label="check"+str(i), callback=checkbox_clic_callback, before="end", tag=id_tag)
     new_button = dpg.add_button(label="Download stream", callback=download_stream_callback,  before="end", tag="download_button")
 
 
@@ -48,7 +49,7 @@ with dpg.window(tag="pw", label="Youtube downloader"):
     dpg.add_input_text(default_value="https://", callback=save_link_callback)
     dpg.add_button(label="Click here to check the streams", callback=check_streams_callback, tag="stream_button")
     dpg.add_text("", tag="end")
-#    dpg.add_button(label="Download stream", callback=download_stream_callback, tag="download_button")
+    dpg.add_button(label="Download stream", callback=download_stream_callback, tag="download_button")
 
 
 
